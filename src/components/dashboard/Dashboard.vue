60f2cd2e12253747af64a7801a4ae4b4b24a178a
Alexsander <mercusar@gmail.com>
2020-03-11 18:11:20 +0200

    add changes on dashboard

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -5,12 +5,47 @@
         >
             <v-toolbar-title>Your notes</v-toolbar-title>
         </v-app-bar>
+        <v-layout column mt-8>
+            <v-layout row wrap justify-space-between ml-2>
+                <v-flex xs12 sm4 ml-4>
+                    <v-text-field
+                        type="text"
+                        label="Search"
+                    >
+                    </v-text-field>
+                </v-flex>
+                <v-flex sm6 mr-10>
+                    <v-layout row align-center>
+                        <v-select
+                            :items="getFilterItems"
+                        />
+                        <v-btn>Add Note</v-btn>
+                    </v-layout>
+                </v-flex>
+            </v-layout>
+
+            <v-layout row wrap ml-2 mr-2>
+                <v-flex  v-for="i in 10">
+                    <NoteCard/>
+                </v-flex>
+            </v-layout>
+        </v-layout>
     </v-app>
 </template>
 
 <script>
+    import NoteCard from "@/components/dashboard/NoteCard";
+
     export default {
-        name: "Dashboard"
+        name: "Dashboard",
+        components: {NoteCard},
+        data: () => ({
+        }),
+        computed:{
+            getFilterItems(){
+                return ["Red", "Green", "Blue"]
+            }
+        }
     }
 </script>
 
---
+++ src/components/dashboard/NoteCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-hover  v-slot:default="{ hover }">
+        <v-card class="note-card"
+            :elevation="hover ? 16 : 6"
+            @click="openNoteModal"
+        >
+            <v-card-title>
+                {{title}}
+                <v-spacer></v-spacer>
+                <span :style="{color: color}">
+                    <v-icon class="inline-icon" :color="color">brightness_1</v-icon>
+                </span>
+            </v-card-title>
+            <v-card-text>
+                {{text}}
+            </v-card-text>
+        </v-card>
+    </v-hover>
+</template>
+
+<script>
+    export default {
+        name: "NoteCard",
+        props:{
+            note: Object,
+            click: Function
+        },
+        data: () => ({
+            created: '',
+            notify_at: '',
+            title: 'Note',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+            color: '#f0252b'
+        }),
+        methods: {
+            openNoteModal(){
+                alert('hello')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.note-card{
+    margin: 6px;
+    max-width: 300px;
+    max-height: 400px;
+}
+</style>
